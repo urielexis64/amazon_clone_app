3f59bfa1532a84f86a7fad17a8c119dcee207f80
Uriel Aispuro <urielalexis64@gmail.com>
2022-06-17 23:18:11 -0600

    add sign in and sign up endpoints

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
+const auth = require("../middlewares/auth");
 const authRouter = express.Router();
 
 authRouter.post("/api/signup", async (req, res) => {
@@ -22,6 +24,65 @@ authRouter.post("/api/signup", async (req, res) => {
 
 		user = await user.save();
 		res.json(user);
+	} catch (err) {
+		res.status(500).json({error: err.message});
+	}
+});
+
+authRouter.post("/api/signin", async (req, res) => {
+	try {
+		const {email, password} = req.body;
+
+		console.log(email, password);
+
+		if (!email || !password) {
+			return res.status(400).json({message: "Please provide email and password"});
+		}
+
+		const user = await User.findOne({email});
+		if (!user) {
+			return res.status(400).json({message: "User does not exists"});
+		}
+
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+		if (!isPasswordValid) {
+			return res.status(400).json({message: "Invalid password"});
+		}
+
+		const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+		res.json({token, ...user._doc});
+	} catch (err) {
+		res.status(500).json({error: err.message});
+	}
+});
+
+authRouter.post("/validateToken", async (req, res) => {
+	try {
+		const token = req.header("x-auth-token");
+		if (!token) {
+			return res.res.json(false);
+		}
+
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		if (!decoded) return res.json(false);
+
+		const user = await User.findById(decoded.id);
+		if (!user) {
+			return res.json(false);
+		}
+
+		res.json(true);
+	} catch (err) {
+		res.status(500).json({error: err.message});
+	}
+});
+
+// get user data
+authRouter.get("/", auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user);
+		res.json({...user._doc, token: req.token});
 	} catch (err) {
 		res.status(500).json({error: err.message});
 	}
