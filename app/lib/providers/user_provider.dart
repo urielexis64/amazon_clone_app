4c654ac2bc75bc60c4ec5d7a6f164e31176aab70
Uriel Aispuro <urielalexis64@gmail.com>
2022-06-17 23:18:38 -0600

    create UserProvider class

---
+++ app/lib/providers/user_provider.dart
@@ -0,0 +1,13 @@
+import 'package:amazon_clone/models/user.dart';
+import 'package:flutter/cupertino.dart';
+
+class UserProvider extends ChangeNotifier {
+  User _user = User.empty();
+
+  User get user => _user;
+
+  void setUser(String user) {
+    _user = User.fromJson(user);
+    notifyListeners();
+  }
+}
