36aa2bebe896bcddddf50bf15999da9feeab57a0
Uriel Aispuro <urielalexis64@gmail.com>
2022-06-25 00:03:41 -0600

    create bottom bar screen

--- app/lib/features/auth/services/auth_service.dart
+++ app/lib/features/auth/services/auth_service.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 
+import 'package:amazon_clone/common/widgets/bottom_bar.dart';
 import 'package:amazon_clone/constants/error_handling.dart';
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/constants/utils.dart';
-import 'package:amazon_clone/features/home/screens/home_screen.dart';
 import 'package:amazon_clone/models/user.dart';
 import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:flutter/material.dart';
@@ -60,7 +60,7 @@ class AuthService {
             context.read<UserProvider>().setUser(response.body);
             await prefs.setString(
                 'x-auth-token', jsonDecode(response.body)['token']);
-            Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+            Navigator.pushReplacementNamed(context, BottomBar.routeName);
           });
     } catch (e) {
       showSnackBar(context, message: e.toString());
